49b60c05e3804813cda1959cd3c222411c273894
Zach Tucker <zct8002@rit.edu>
2020-06-28 12:09:46 -0400

    got reactions working

--- Bot.py
+++ Bot.py
@@ -2,9 +2,10 @@ import discord
 
 botVars = dict()
 bannedWords = list()
+reactionData = dict()
 
 
-def importSettings():
+def import_settings():
     settings = open("settings.txt", "r")
     banned = open("bannedwords.txt", "r")
     global bannedWords
@@ -45,6 +46,7 @@ class MyClient(discord.Client):
     global botVars
 
     async def on_ready(self):
+        print('------')
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
@@ -54,36 +56,57 @@ class MyClient(discord.Client):
         author = message.author
         content = message.content
         channel = message.channel
-        prefixLength = len(botVars.get("prefix"))
+        guild = message.guild
+        emoji_list = guild.emojis
+
+        prefix = botVars.get("prefix")
 
         # we do not want the bot to reply to itself
         if author.id == self.user.id:
             return
-
+        # check for banned words being used
         for word in bannedWords:
             if word.lower() in content.lower():
                 await message.delete()
-                await channel.send("Boop!")
+                await channel.send("Boop!", delete_after=5)
                 await author.send(
-                    "Sorry friend, you used a banned word, the word was \"" +
-                    word + "\". if you feel this was done in error, please contact an administrator")
-                return ""
+                    "Sorry friend, you used a banned word, the message you sent was deleted. " +
+                    " if you feel this was done in error, please contact an administrator")
+                await author.send(
+                    "the message that contained the word was: \"" + content + "\", the word was \"" + word + "\"")
+                return
+        # check for actual commands being used
+        if message.content[:len(prefix)] == prefix:
+            realContent = message.content[len(prefix):].lower().strip().split(" ")
+            if realContent[0] == 'help':
+                await message.delete()
+                await channel.send()
+            elif realContent[0] == 'clear':
+                await message.delete()
+                messages = await channel.history(limit=int(realContent[1])).flatten()
+                await channel.delete_messages(messages)
+            elif realContent[0] == "react":
+                messageSent = await channel.send("Testing shit")
+                for emoji in emoji_list:
+                    await messageSent.add_reaction(emoji)
 
-        if message.content[:prefixLength] == (botVars.get("prefix")):
-            realContent = message.content[prefixLength:]
-            if realContent == "help":
-                print("")
+    async def on_reaction_add(self, reaction, user):
+        if user.id == self.user.id:
+            return
+        else:
+            reactionData[user] = list(reaction.emoji)
+            return
 
 
-def runBot():
+def run_bot():
     client = MyClient()
     client.run(botVars.get("token"))
 
 
-def main():
-    importSettings()
-    runBot()
+def init():
+    import_settings()
+    run_bot()
 
 
 if __name__ == "__main__":
-    main()
+    init()
